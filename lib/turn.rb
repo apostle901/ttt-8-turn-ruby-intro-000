f75fd3594d85ed6e7716167b725d00026d553b9d
Jawann <jawann@gmail.com>
2017-10-22 22:46:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,12 @@
-def move
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  return -1 if user_input < 1 || user_input > 9
+  user_input - 1
+end
+
+def move(board, position, which_char='X')
+  board[position] = which_char
+  board
 end
 
 def display_board(game)
@@ -17,4 +25,26 @@ end
 
 def position_taken?(board, index)
   board[index] == 'X' || board[index] == 'O' ? true : false
+end
+ask for input
+get input
+convert input to index
+if index is valid
+  make the move for index
+  show the board
+else
+  ask for input again until you get a valid input
+end
+
+def turn
+  loop do
+    puts "Please enter 1-9:"
+    pick = gets.strip
+    index = input_to_index(pick)
+    if valid_move?(board, index)
+      move(board, index, 'X')
+      display_board(board)
+      break
+    end
+  end
 end
